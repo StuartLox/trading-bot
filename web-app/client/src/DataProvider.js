a82b6cf3869efa231fcae66bcb00b6768fc4eeb2
StuartLox <stuart.loxton@xinja.com.au>
2020-03-26 00:04:17 +1100

    Add chart component

--- web-app/client/src/DataProvider.js
+++ web-app/client/src/DataProvider.js
@@ -1,7 +1,16 @@
 export function getInitialPriceData() {
-  return [{
-    symbol: 'BTC-AUD',
-    timestamp: '2020-03-22T10:25',
-    averagePrice: '6272.85'
-  }]
+  return {
+    labels: [],
+    datasets: [
+        {
+            label: 'Bitcoin',
+            backgroundColor: 'rgba(255,99,132,0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+            hoverBorderColor: 'rgba(255,99,132,1)',
+            data: []
+        }
+    ]
+};
 }
---
+++ web-app/client/src/components/Chart.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Line } from 'react-chartjs-2';
+import { getInitialPriceData } from '../DataProvider';
+
+
+class Chart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: getInitialPriceData()
+    };
+    this.eventSource = new EventSource('http://localhost:5000/events');
+  }
+
+  componentDidMount() {
+    this.eventSource.addEventListener('priceStateUpdate', (e) =>
+      this.updatePriceState(JSON.parse(e.data)));
+  }
+
+  updatePriceState(priceState) {
+    const date = new Date(priceState.timestamp).toLocaleTimeString()
+    const averagePrice = parseFloat(priceState.averagePrice).toFixed(2)
+    this.state.data.labels.push(date)
+    this.state.data.datasets[0].data.push(averagePrice)
+    this.setState(Object.assign({ data: this.state.data }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Average Bitcoin Price</h2>
+        <Line
+          data={this.state.data}
+          width={1500}
+          height={500}
+          options={{
+            maintainAspectRatio: false
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export default Chart;
